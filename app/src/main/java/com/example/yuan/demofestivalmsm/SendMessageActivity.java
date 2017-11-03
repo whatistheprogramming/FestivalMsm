e1cb3eab2791a4ba724ed88c65d871b0c2a99438
yuan <491187718@qq.com>
2017-11-03 21:32:25 +0800

    完成获取联系人的姓名和号码“

--- app/src/main/java/com/example/yuan/demofestivalmsm/SendMessageActivity.java
+++ app/src/main/java/com/example/yuan/demofestivalmsm/SendMessageActivity.java
@@ -1,22 +1,33 @@
 package com.example.yuan.demofestivalmsm;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.yuan.demofestivalmsm.bean.Festival;
 import com.example.yuan.demofestivalmsm.bean.FestivalLab;
 import com.example.yuan.demofestivalmsm.bean.Msg;
 import com.example.yuan.demofestivalmsm.view.FlowLayout;
 
+import java.util.HashSet;
+
 public class SendMessageActivity extends AppCompatActivity
 {
 
+    private static final int CODE_RESULT = 1;
     //节日id
     private int mFestivalId;
 
@@ -35,7 +46,16 @@ public class SendMessageActivity extends AppCompatActivity
 
     private FloatingActionButton mFabSend;
 
+    //加载进度条的view
     private View mLayoutLoading;
+
+    //存放选中联系人的姓名
+    private HashSet<String> mContactNames = new HashSet<>();
+    //选中联系人的电话
+    private HashSet<String> mContactNumber = new HashSet<>();
+
+    private LayoutInflater mInflater;
+
 
 
     @Override
@@ -44,11 +64,74 @@ public class SendMessageActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_message);
 
+        mInflater = LayoutInflater.from(this);
+
         //获取id数据
         initData();
 
         //初始化控件
         initViews();
+
+        //事件的初始化
+        initEvent();
+    }
+
+    private void initEvent()
+    {
+        mBtnAdd.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //跳转到联系人
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent,CODE_RESULT);
+            }
+        });
+    }
+
+    /**
+     * 当选择联系人后会回调这个方法
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        switch (requestCode)
+        {
+            case CODE_RESULT:
+                if (resultCode == RESULT_OK)
+                {
+                    //获取联系人的姓名和号码
+                    Uri contactUri = data.getData();
+                    Cursor cursor = getContentResolver().query(contactUri, null, null, null, null);
+                    cursor.moveToFirst();
+                    //联系人的姓名
+                    String contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                    //联系人的号码
+                    String number = getContactNumber(cursor);
+                    if (!TextUtils.isEmpty(number))
+                    {
+                        mContactNumber.add(number);
+                        mContactNames.add(contactName);
+
+                        //显示添加的联系人
+                        addTag(contactName);
+                    }
+
+
+
+                }
+        }
+    }
+
+    private void addTag(String contactName)
+    {
+        TextView view = (TextView) mInflater.inflate(R.layout.tag, mFLContacts, false);
+        view.setText(contactName);
+        //加入到FlowLayout中
+        mFLContacts.addView(view);
+
     }
 
     private void initViews()
@@ -61,7 +144,7 @@ public class SendMessageActivity extends AppCompatActivity
 
         //设置加载进度条不可见
         mLayoutLoading.setVisibility(View.GONE);
-        
+
         //如果不是自定义短信内容
         if (msgId != -1)
         {
@@ -91,5 +174,28 @@ public class SendMessageActivity extends AppCompatActivity
         intent.putExtra("festival_id", festivalId);
         intent.putExtra("msg_id", msgId);
         context.startActivity(intent);
+    }
+
+    /**
+     * 获取联系人的号码
+     */
+    public String getContactNumber(Cursor cursor)
+    {
+        int numberCount = cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+        String number = null;
+        //如果有电话号码
+        if (numberCount > 0)
+        {
+            int contactId = cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            Cursor phoneCursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactId, null, null);
+            phoneCursor.moveToFirst();
+            //有多个号码，选第一个
+            number = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+            phoneCursor.close();
+        }
+
+        cursor.close();
+        return number;
     }
 }
